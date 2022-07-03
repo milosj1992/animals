5e3388eadb913f43c92341d0e8a43abc9867735c
milosj1992 <milosje1992@gmail.com>
2022-07-03 11:33:59 +0200

    animals and redirect from animal root

--- src/components/Animals.js
+++ src/components/Animals.js
@@ -2,17 +2,17 @@ import UserService from "../services/user.service";
 import {useEffect, useState} from "react";
 
 const styles = {
-    animalWrapper:{
-        display:"flex",
-        flexFlow:"column",
-        gap:"10px",
-        flexWrap:"wrap"
+    animalWrapper: {
+        display: "flex",
+        flexFlow: "column",
+        gap: "10px",
+        flexWrap: "wrap"
     },
     animalinMap: {
         display: "flex",
         justifyContent: "flex-start",
-        gap:"10px",
-        border : "3px solid blue",
+        gap: "10px",
+        border: "3px solid blue",
         padding: "5px",
         borderRadius: "4px"
     }
@@ -39,9 +39,12 @@ export const Animals = () => {
                         <div>type: {item.type}</div>
                         <div>Origin: {item.origin}</div>
                     </div>)
-            }) : <div>
+            }) :
+            animalLength!=null?<div>Animals loading...</div>:
+            <div>
                 <p>you have to login first</p>
                 <a href="/auth/login">here</a>
-            </div>}
+                </div>
+        }
     </div>)
 }
