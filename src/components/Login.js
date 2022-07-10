95e292b7b4caf064fb01dd4820392a6110ed508b
milosj1992 <milosje1992@gmail.com>
2022-07-10 05:05:04 +0200

    validation added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,37 @@
 import AuthService from "../services/auth.service";
-import {useEffect, useState} from "react";
+import {useEffect, useState, useRef} from "react";
 import {useNavigate} from "react-router-dom";
 import UserService from "../services/user.service";
+import Form from 'react-validation/build/form';
+import Input from 'react-validation/build/input';
+import CheckButton from "react-validation/build/button";
+import validator from 'validator';
+
+
+const required = value => {
+    if (!value) {
+        return (
+            <div>
+                This field is required!
+            </div>
+        );
+    }
+};
+const vpassword = value => {
+    if (value.length < 6 || value.length > 40) {
+        return (
+            <div>
+                The password must be between 6 and 40 characters.
+            </div>
+        );
+    }
+};
+const emailvalidation = (value) => {
+    if (!validator.isEmail(value)) {
+        return `${value} is not a valid email.`
+    }
+};
+
 
 export const Login = () => {
     const navigate = useNavigate();
@@ -10,11 +40,15 @@ export const Login = () => {
     const [password, setPassword] = useState('');
     const [checkanimals, setCheckAnimals] = useState([]);
 
+    const currentForm = useRef();
+    const afterForm = useRef();
+
     useEffect(() => {
         if (checkanimals.items != undefined || localStorage.getItem("user")) {
             navigate("/animals");
         }
     }, [checkanimals]);
+
 
     const getAnima = () => {
         UserService.getAnimals(1)
@@ -29,21 +63,35 @@ export const Login = () => {
     const updatePassword = (e) => {
         setPassword(e.target.value);
     }
-    const handleLogin = () => {
-        AuthService.login(email, password).then(r => {
-            if (r.token) {
-                getAnima();
-            }
-        });
+    const handleLogin = (e) => {
+        e.preventDefault();
+        currentForm.current.validateAll();
+
+        if (afterForm.current.context._errors.length === 0) {
+            AuthService.login(email, password)
+                .then(r => {
+                    if (r.token) {
+                        getAnima();
+                    }
+                });
+        }
     }
     return (
-        <div>
-            <div className="input-credit">
-                <input className="email" placeholder="Email" onChange={updateEmail} value={email || ""}/>
-                <input className="password" placeholder="Password" type="password" onChange={updatePassword}
-                       value={password || ""}/>
-                <button onClick={() => handleLogin()}>Login</button>
+        <Form onSubmit={(e) => handleLogin(e)}
+              ref={currentForm}>
+            <div>
+                <div className="input-credit">
+                    <Input className="email" placeholder="Email" validations={[required, emailvalidation]}
+                           onChange={updateEmail} value={email || ""} name="email"/>
+                    <Input className="password" placeholder="Password" type="password" onChange={updatePassword}
+                           value={password || ""} validations={[required, vpassword]} name="password"/>
+                    <button>Submit</button>
+                </div>
             </div>
-        </div>
+            <CheckButton
+                style={{display: "none"}}
+                ref={afterForm}
+            />
+        </Form>
     )
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,55 @@
 import AuthService from "../services/auth.service";
-import {useState} from "react";
+import {useState, useRef} from "react";
 import {useNavigate} from "react-router-dom";
+import Form from 'react-validation/build/form';
+import Input from 'react-validation/build/input';
+import CheckButton from "react-validation/build/button";
+import validator from 'validator';
+
+
+const required = value => {
+    if (!value) {
+        return (
+            <div>
+                This field is required!
+            </div>
+        );
+    }
+};
+
+const vusername = value => {
+    if (value.length < 3 || value.length > 20) {
+        return (
+            <div className="alert alert-danger" role="alert">
+                The username must be between 3 and 20 characters.
+            </div>
+        );
+    }
+};
+
+const vpassword = value => {
+    if (value.length < 6 || value.length > 40) {
+        return (
+            <div>
+                The password must be between 6 and 40 characters.
+            </div>
+        );
+    }
+};
+const emailvalidation = (value) => {
+    if (!validator.isEmail(value)) {
+        return `${value} is not a valid email.`
+    }
+};
 
 export const Register = () => {
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+
+    const currentForm = useRef();
+    const afterForm = useRef();
 
     const updateUsername = (e) => {
         setUsername(e.target.value);
@@ -17,22 +60,36 @@ export const Register = () => {
     const updatePassword = (e) => {
         setPassword(e.target.value);
     }
-    const handleRegister = () => {
-        AuthService.register(email, username, password)
-            .then(r => {
-                if(r.status==201){
-                    navigate("/auth/login");
-                }
-            });
+    const handleRegister = (e) => {
+        e.preventDefault();
+        currentForm.current.validateAll();
+        if (afterForm.current.context._errors.length === 0) {
+            AuthService.register(email, username, password)
+                .then(r => {
+                    if (r.status == 201) {
+                        navigate("/auth/login");
+                    }
+                });
+        }
     }
     return (
-        <div>
-            <div className="input-credit">
-                <input className="email" placeholder="Email" onChange={updateEmail} value={email || ""}/>
-                <input className="username" placeholder="Username" onChange={updateUsername} value={username || ""}/>
-                <input className="password" placeholder="Password" type="password" onChange={updatePassword} value={password || ""}/>
-                <button onClick={() => handleRegister()}>Register</button>
+        <Form onSubmit={(e) => handleRegister(e)} ref={currentForm}>
+            <div>
+                <div className="input-credit">
+                    <Input className="email" placeholder="Email" onChange={updateEmail} value={email || ""}
+                           validations={[required, emailvalidation]} name="email"/>
+                    <Input className="username" placeholder="Username" onChange={updateUsername}
+                           value={username || ""} validations={[required, vusername]} name="username"/>
+                    <Input className="password" placeholder="Password" type="password" onChange={updatePassword}
+                           value={password || ""} validations={[required, vpassword]} name="password"/>
+
+                </div>
+                <button>Submit</button>
             </div>
-        </div>
+            <CheckButton
+                style={{display: "none"}}
+                ref={afterForm}
+            />
+        </Form>
     )
 }
